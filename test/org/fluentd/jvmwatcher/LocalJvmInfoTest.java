c268ad5ffa544272fe483a8b1134012b31d500b8
Masayuki Miyake <masayukimiyake97@gmail.com>
2013-04-28 16:54:03 +0900

    edit test case.

--- test/org/fluentd/jvmwatcher/LocalJvmInfoTest.java
+++ test/org/fluentd/jvmwatcher/LocalJvmInfoTest.java
@@ -10,103 +10,29 @@ import org.junit.Test;
 
 public class LocalJvmInfoTest
 {
-    Map<Integer, LocalJvmInfo>      jvmMap_ = null;
-
-    @Before
-    public void setUp() throws Exception
-    {
-        this.jvmMap_ = LocalJvmInfo.getAllLocalJvmInfos();
-        
-    }
 
     @Test
     public void testStartManagementAgent()
     {
-        for(Map.Entry<Integer, LocalJvmInfo> elem : this.jvmMap_.entrySet())
+        Map<Integer, LocalJvmInfo>      jvmMap = LocalJvmInfo.getAllLocalJvmInfos();
+        for(Map.Entry<Integer, LocalJvmInfo> elem : jvmMap.entrySet())
         {
             int             key = elem.getKey();
             LocalJvmInfo    val = elem.getValue();
 
             try
             {
+                System.out.println("key=" + key + " isManageable=" + val.isManageable() +
+                                   " isAttachSupported=" + val.isAttachSupported() +
+                                   " isCommandLine=" + val.getCommandLine_() +
+                                   " address=" + val.getAddress());
+                
                 val.startManagementAgent();
             }
             catch (IOException e)
             {
                 e.printStackTrace();
             }
-        }
-    }
-
-    @Test
-    public void testGetAddress()
-    {
-        for(Map.Entry<Integer, LocalJvmInfo> elem : this.jvmMap_.entrySet())
-        {
-            int             key = elem.getKey();
-            LocalJvmInfo    val = elem.getValue();
-            
-            System.out.println("pid=" + key + " get address =" + val.toString());
-        }
-    }
-
-    @Test
-    public void testGetCommandLine_()
-    {
-        for(Map.Entry<Integer, LocalJvmInfo> elem : this.jvmMap_.entrySet())
-        {
-            int             key = elem.getKey();
-            LocalJvmInfo    val = elem.getValue();
-            
-            System.out.println("pid=" + key + " command line=" + val.getCommandLine_());
-        }
-    }
-
-    @Test
-    public void testGetDisplayName()
-    {
-        for(Map.Entry<Integer, LocalJvmInfo> elem : this.jvmMap_.entrySet())
-        {
-            int             key = elem.getKey();
-            LocalJvmInfo    val = elem.getValue();
-            
-            System.out.println("pid=" + key + " DisplayName=" + val.getDisplayName());
-        }
-    }
-
-    @Test
-    public void testGetVmid()
-    {
-        for(Map.Entry<Integer, LocalJvmInfo> elem : this.jvmMap_.entrySet())
-        {
-            int             key = elem.getKey();
-            LocalJvmInfo    val = elem.getValue();
-            
-            System.out.println("pid=" + key + " vmid=" + val.getJvmid());
-        }
-    }
-
-    @Test
-    public void testIsAttachSupported()
-    {
-        for(Map.Entry<Integer, LocalJvmInfo> elem : this.jvmMap_.entrySet())
-        {
-            int             key = elem.getKey();
-            LocalJvmInfo    val = elem.getValue();
-            
-            System.out.println("pid=" + key + " AttachSupported=" + val.isAttachSupported());
-        }
-    }
-
-    @Test
-    public void testToString()
-    {
-        for(Map.Entry<Integer, LocalJvmInfo> elem : this.jvmMap_.entrySet())
-        {
-            int             key = elem.getKey();
-            LocalJvmInfo    val = elem.getValue();
-            
-            System.out.println("pid=" + key + " val=" + val.toString());
         }
     }
 
